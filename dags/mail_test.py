9d971e2e2d30766390125de792ca9e833fdef502
AmirZahre <zahreddi@ualberta.ca>
2022-04-08 13:11:27 -0600

    email

---
+++ dags/mail_test.py
@@ -0,0 +1,30 @@
+import airflow 
+from datetime import timedelta 
+from airflow import DAG 
+from datetime import datetime, timedelta 
+from airflow.operators.python_operator import PythonOperator 
+from airflow.operators.email_operator import EmailOperator
+
+
+# email_backend = airflow.utils.email.send_email_smtp
+
+with DAG(
+    "databricks_dag",
+    start_date=datetime(2021, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    default_args={
+        "email_on_failure": False,
+        "email_on_retry": False,
+    },
+) as dag:
+
+
+    mail = EmailOperator(
+        task_id='mail',
+        to='amir.zahreddine@astronomer.io',
+        subject='Daily Movers',
+        html_content="""<h3>Email Test</h3>"""
+    )
+
+mail
